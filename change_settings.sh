a6d37d2e8d6bda301912b9e45e7005785502c2a0
Onat Bas <git@onat.me>
2018-11-04 17:07:52 +0300

    update: merged desc and rename to settings

--- change_settings.sh
+++ change_settings.sh
@@ -5,11 +5,26 @@ CONFIG=""
 config=$(cat ~/.config/hub)
 public_token=$(echo $config | sed -E 's/.+onatbas oauth_token: ([a-z0-9]+) .+/\1 /g')
 
-NAME="test"
-
-while getopts :n:s:i:p:w: option
+while getopts :hr:d:s:i:p:w:n: option
 do
 	case "$option" in
+	h)
+		echo "Available options:
+
+-n NAME		: (required) name of the repository
+-r NEW_NAME	: Rename the repo to NEW_NAME
+-s 1/0		: Set repo to private
+-i issues 1/0	: has_issues
+-p projects 1/0	: has_projects
+-w wiki 1/0	: has_wiki"
+		exit
+		;;
+	r)
+		CONFIG=",\"name\": $OPTARG $CONFIG"
+		;;
+	d)
+		CONFIG=",\"description\": $OPTARG $CONFIG"
+		;;
 	s)
 		CONFIG=",\"private\": $OPTARG $CONFIG"
 		;;
@@ -26,15 +41,12 @@ do
 		NAME="$OPTARG"
 		;;
 	*)
-		echo "yanlis biseyler oldu haci"
+		echo "Yanlis komut. -h yap haci"
+		exit
 		;;
 	esac
 done
 
-
 obj="{ \"name\": \"$NAME\" $CONFIG }"
-
-
-echo $obj
 
 curl -X PATCH -d "$obj" https://api.github.com/repos/$user/$NAME -H "authorization: Bearer $public_token"
