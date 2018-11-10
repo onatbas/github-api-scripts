68cf6601ac55f65d9c70e11080846132a2f2b3b8
Onat Bas <git@onat.me>
2018-11-10 14:10:13 +0300

    add: repo owner option

--- settings.sh
+++ settings.sh
@@ -5,7 +5,7 @@ CONFIG=""
 config=$(cat ~/.config/hub)
 public_token=$(echo $config | sed -E 's/.+onatbas oauth_token: ([a-z0-9]+) .+/\1 /g')
 
-while getopts :hr:d:s:i:p:w:n: option
+while getopts :hr:u:d:s:i:p:w:n: option
 do
 	case "$option" in
 	h)
@@ -16,26 +16,27 @@ do
 -s 1/0		: Set repo to private
 -i issues 1/0	: has_issues
 -p projects 1/0	: has_projects
+-u REPO_OWNER	: repo owner, can be user or organization name. Default is $user
 -w wiki 1/0	: has_wiki"
 		exit
 		;;
 	r)
-		CONFIG=",\"name\": $OPTARG $CONFIG"
+		CONFIG=",\"name\": \"$OPTARG\" $CONFIG"
 		;;
 	d)
-		CONFIG=",\"description\": $OPTARG $CONFIG"
+		CONFIG=",\"description\": \"$OPTARG\" $CONFIG"
 		;;
 	s)
-		CONFIG=",\"private\": $OPTARG $CONFIG"
+		CONFIG=",\"private\": \"$OPTARG\" $CONFIG"
 		;;
 	i)
-		CONFIG=",\"has_issues\": $OPTARG $CONFIG"	
+		CONFIG=",\"has_issues\": \"$OPTARG\" $CONFIG"	
 		;;
 	p)
-		CONFIG=",\"has_projects\": $OPTARG $CONFIG"	
+		CONFIG=",\"has_projects\": \"$OPTARG\" $CONFIG"	
 		;;
 	w)
-		CONFIG=",\"has_wiki\": $OPTARG $CONFIG"	
+		CONFIG=",\"has_wiki\": \"$OPTARG\" $CONFIG"	
 		;;
 	n)
 		NAME="$OPTARG"
@@ -47,6 +48,6 @@ do
 	esac
 done
 
-obj="{ \"name\": \"$NAME\" $CONFIG }"
+obj="{ \"name\": \"$NAME\" "$CONFIG" }"
 
 curl -X PATCH -d "$obj" https://api.github.com/repos/$user/$NAME -H "authorization: Bearer $public_token"
