a4f9f0a47b44889f50b87c9551ae7568af97f45e
Onat Bas <git@onat.me>
2018-11-17 16:10:10 +0300

    add: p, s parameters added for public/private

--- list.sh
+++ list.sh
@@ -2,7 +2,30 @@ user=onatbas
 config=$(cat ~/.config/hub)
 public_token=$(echo $config | sed -E 's/.+onatbas oauth_token: ([a-z0-9]+) .+/\1 /g')
 
+while getopts :psh option
+do
+	case "$option" in
+	p)
+		visibility="&visibility=public"
+		;;
+	s)
+		visibility="&visibility=private"
+		;;
+	h)
+		echo "default: list all repos
+p: 	get only public repos
+s:	get only private repos"
+		exit
+		;;
+	*)
+		echo "olmadi... Use -h for help"
+		exit
+		;;
+	esac
+done
+
+# Get correct api tokens
 
 
-curl -s "https://api.github.com/user/repos?affiliation=owner&visibility=public" -H "authorization: Bearer $public_token"
+curl -s "https://api.github.com/user/repos?affiliation=owner$visibility" -H "authorization: Bearer $public_token"
 
